2630f27cef83e544f1dd1d48eac0246d7d5aa6b9
codeviith <codeviith@Cinya.>
2023-10-11 10:33:42 -0400

    cc-client-server

--- flask/cc/client_server/starter/app.py
+++ flask/cc/client_server/starter/app.py
@@ -16,11 +16,111 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
+
 @app.get("/")
 def index():
     return "client/server"
 
 
+
+@app.get("/clients")
+def get_clients():
+    '''
+    for each client:
+        convert client to dict
+        get all servers for client (convert to dict)
+        add server dict list to client json
+        add final result to list
+    '''
+    # Query the database following our request to get all our data
+    clients = Client.query.all()
+    print(clients) # [<Client123434>, <Client448920384>, ...]
+
+    # Structure the data into relevant JSON to return to frontend
+    # A.K.A. Convert Pythonic data into JavaScript-readable data
+    client_json_list = []
+    for client in clients:
+        # print(client.name)
+        client_dict = client.to_dict()
+
+        server_dict_list = []
+        for server in client.servers:
+            server_dict_list.append(server.to_dict())
+        # server_dict_list = [server.to_dict() for server in client.servers]
+        
+        client_dict["servers"] = server_dict_list
+        client_json_list.append(client_dict)
+        # {'kash': [{'jacket': ..., 'hat': ...}],
+        #  'chett': [{'coat'}],
+        #  'daniel': [{}]}
+    return make_response(jsonify(client_json_list), 200)
+
+
+
+@app.post('/servers')
+def post_server():
+    data = request.json
+
+    try:
+        server = Server(name=data.get('name'))
+        db.session.add(server)
+        db.session.commit()
+        return make_response(jsonify(server.to_dict(rules="-messages")), 201)
+    except:
+        return make_response(jsonify({"Error": "Could not creater server!"}), 405)
+        
+
+
+@app.post('/messages')
+def post_message():
+    data = request.get_json()
+
+    try:
+        client = db.session.get(Client.data.get("client_id"))
+        if not client:
+            return make_response(jsonify({"Error": "No such client"}), 404)
+        server = db.session.get(Server.data.get("server_id"))
+        if not server:
+            return make_response(jsonify({"Error": "No such Server"}), 404)
+        msg = Message(
+            client=client,
+            server=server,
+            content=data.get("content"),
+        )
+        db.session.add(msg)
+        db.session.commit()
+        return make_response(jsonify(msg.to_dict(rules=("-client_id", "-server_id"))))
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({"Error": "Could not send message."}), 405)
+
+
+
+@app.patch('/messages/<int:id>')
+def patch_message(id):
+    data = request.json
+    msg = db.session.get(Message, id)
+    if not msg:
+        return make_response(jsonify({"Error": "no such message"}), 404)
+    try:
+        for key in data:
+            setattr(msg, key, data[key])
+        db.session.add(msg)
+        db.session.commit()
+        return make_response(jsonify(msg.to_dict()))
+    except:
+        return make_response(jsonify({"Error": "No such message"}), 405)
+
+
+
+@app.delete('/messages/<int:id>')
+def delete_msg(id):
+    msg = db.session.get(Message, id)
+    if not msg:
+        return make_response(jsonify({"Error": "no such message"}), 404)
+    db.session.delete(msg)
+    db.session.commit()
+    return make_response(jsonify({}), 200)
 
 
 
--- flask/cc/client_server/starter/models.py
+++ flask/cc/client_server/starter/models.py
@@ -17,17 +17,70 @@ db = SQLAlchemy(metadata=metadata)
 class Server(db.Model, SerializerMixin):
     __tablename__ = "server_table"
 
+    serialize_rules = ("-message_list.servers_obj",)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    message_list = db.relationship("Message", back_populates="servers_obj")
+
+    clients = association_proxy("message_list", "clients_obj")
+
+    @validates("name")
+    def validate_name(self, key, name):
+        if not (name.isupper() and name.isalpha()):
+            raise ValueError("Server name must all be capitalized!")
+        else:
+            return name
+
+
+
 
 
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = "message_table"
 
+    serialize_rules = ("-servers_obj.message_list", "-clients_obj.message_list")
+
     id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey("server_table.id"), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey("client_table.id"), nullable=False)
+
+    servers_obj = db.relationship("Server", back_populates="message_list")
+    clients_obj = db.relationship("Client", back_populates="message_list")
+
+    @validates("content")
+    def validate_name(self, key, content):
+        if len(string(content)):
+            raise ValueError("Content must not be empty")
+        return content
+
+
+
+
 
 
 class Client(db.Model, SerializerMixin):
     __tablename__ = "client_table"
+
+    serialize_rules = ("-message_list.clients_obj",)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    message_list = db.relationship("Message", back_populates="clients_obj" )
+
+    servers = association_proxy("message_list", "servers_obj")
+
+    @validates("name")
+    def validate_name(self, key, name):
+        if not (len(name) >= 5 and len(name) <= 15):
+            raise ValueError("Client name must be between 5 and 15 characters, inclusive!")
+        else:
+            return name
+
+
+
 
