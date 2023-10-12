d88a615e1ec3f10da8d9d9958f6284b607ad04ef
codeviith <codeviith@Cinya.>
2023-10-12 13:42:54 -0400

    cc lmr

--- flask/cc/lmr/starter/app.py
+++ flask/cc/lmr/starter/app.py
@@ -16,9 +16,73 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
+
 @app.get("/")
 def index():
     return "right/left"
+
+
+@app.get('/rights')
+def get_rights():
+    rights = Right.query.all()
+    data = [right.to_dict(rules=("-middle_list",)) for right in rights]
+
+    return make_response(jsonify(data), 200)
+
+
+@app.get('/rights/<int:id>')
+def get_rights_by_id(id):
+    right = Right.query.filter(Right.id == id).first()
+    ### rights = db.session.get(Right, id)
+    if not right: # So is rights a boolean??
+        return make_response(jsonify({"Error":"No such data for Right."}), 404)
+    return make_response(jsonify(right.to_dict()), 200)
+
+
+@app.post('/lefts')
+def post_lefts():
+    data = request.json
+
+    try:
+        left = Left(column=data.get('column'))
+        db.session.add(left)
+        db.session.commit()
+        return make_response(jsonify(left.to_dict(), 200))
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({"Error": "Invalid data: Can't post Left"}), 405)
+
+
+@app.patch('/middles/<int:id>')
+def patch_middles_by_id(id):
+    data = request.json
+
+    middle_to_patch = db.session.get(Middle, id)
+    if not middle_to_patch:
+        return make_response(jsonify({"Error": "No such data for Middle."}), 404)
+    
+    try:
+        for key in data:
+            setattr(middle_to_patch, key, data[key])
+            db.session.add(middle_to_patch)
+            db.session.commit()
+            return make_response(jsonify(middle_to_patch.to_dict(rules=("-left", "-right"))), 201)
+    except Exception as e:
+        print(e)
+        return make_response(set({"Error": "Cannot patch Middle."}), 405)
+
+
+@app.delete('/lefts/<int:id>')
+def delete_left_by_id(id):
+    left = db.session.get(Left, id)
+
+    if not left:
+        return make_response(jsonify({"Error": "No such data for Left."}), 404)
+    db.session.delete(left)
+    db.session.commit()
+    return make_response(jsonify({}), 200)
+
+
 
 
 if __name__ == "__main__":
--- flask/cc/lmr/starter/models.py
+++ flask/cc/lmr/starter/models.py
@@ -18,15 +18,70 @@ class Left(db.Model, SerializerMixin):
     __tablename__ = "left_table"
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable=False, unique=True)
+
+    middle_list = db.relationship("Middle", back_populates="left_obj", cascade="all, delete")
+
+    serialize_rules = ("-middle_list.left_obj",)
+
+    rights = association_proxy("middle_list", "right_obj")  ### always has to be 'list', 'object'
+
+    @validates("column")
+    def validate_left_column(self, key, column):
+        if not isinstance(column, string):
+            raise ValueError("Entry must be a string!")
+        else:
+            return column
+
 
 
 class Middle(db.Model, SerializerMixin):
     __tablename__ = "middle_table"
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable=False)
+    left_id = db.Column(db.Integer, db.ForeignKey('left_table.id'), nullable=False) ### attributes with '=' must be placed at the end!!!!
+    right_id = db.Column(db.Integer, db.ForeignKey('right_table.id'),nullable=False)
+
+    left_obj = db.relationship("Left", back_populates="middle_list")
+    right_obj = db.relationship("Right", back_populates="middle_list")
+
+    serialize_rules = ("-left_obj.middle_list", "-right_obj.middle_list")
+
+    @validates("column")
+    def validate_left_column(self, key, column):
+        ### Method 1:
+        for char in column:
+            if not char in string.punctuation: ### shouldn't this be 'if char not in string.punctuation'??
+                raise ValueError("Entry must be a punctuation!")
+        return column
+        
+        ### Method 2:
+        # if len(set(column) - set(string.punctuation)) > 0:
+        #     raise ValueError("Entry must be a punctuation!")
+        # else:
+        #     return column
+        
+        ### Method 3:
+
+
 
 
 class Right(db.Model, SerializerMixin):
     __tablename__ = "right_table"
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.Integer, nullable=False)
+
+    middle_list = db.relationship("Middle", back_populates="right_obj", cascade="all, delete")
+
+    serialize_rules = ("-middle_list.right_obj",)
+
+    lefts = association_proxy("middle_list", "left_obj")  ### always has to be ('list', 'object')
+
+    @validates("column")
+    def validate_left_column(self, key, column):
+        if not isinstance(column, int):
+            raise ValueError("Entry must be numerical values!")
+        else:
+            return column
